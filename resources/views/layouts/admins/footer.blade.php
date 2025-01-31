35c99b1c157eb134ba2baad2c444c3372f346c1a
ziadmuzaffar <ziadmuzaffar@gmail.com>
2025-01-31 21:37:26 +0300

    add version to files

--- resources/views/layouts/admins/footer.blade.php
+++ resources/views/layouts/admins/footer.blade.php
@@ -1,5 +1,5 @@
-        <script src="{{asset('public/assets/lib/jquery_v3.6.3.js')}}"></script>
-        <script src="{{asset('public/assets/lib/bootstrap.bundle.min.js')}}"></script>
-        <script src="{{asset('public/admins/js/style.js')}}"></script>
+        <script src="{{asset('public/assets/lib/jquery.min.js?ver=3.6.3')}}"></script>
+        <script src="{{asset('public/assets/lib/bootstrap.bundle.min.js?ver=5.3.3')}}"></script>
+        <script src="{{asset('public/admins/js/style.js?ver=0.0.1')}}"></script>
     </body>
 </html>
